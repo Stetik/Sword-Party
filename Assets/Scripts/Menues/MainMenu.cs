bc27f4946292f1df487fd072669aaf5622be1dac
Stetik <132297315+Stetik@users.noreply.github.com>
2024-08-22 17:58:49 -0300

    Title + Lobby

--- Assets/Scripts/Menues/MainMenu.cs
+++ Assets/Scripts/Menues/MainMenu.cs
@@ -8,10 +8,16 @@ public class MainMenu : MonoBehaviour
 public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }     
+    }
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("QUIT");
+    }
+
+    public void BackGame()
+    {
+       SceneManager.LoadScene("MainMenu");
+        Debug.Log("pressed");
     }
 }
