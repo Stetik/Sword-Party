b94ee670b479ee34f4dc8939c5a28be35f8f8589
Stetik <132297315+Stetik@users.noreply.github.com>
2024-10-31 19:21:58 -0300

    movement + dash!

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.Runtime.CompilerServices;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    public int speed;
+    [SerializeField] public int speed;
+    [SerializeField] public int JumpForce;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
     private PhotonView pv;
 
@@ -17,25 +30,69 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (pv.IsMine) 
+        if (pv.IsMine)
         {
-            if (Input.GetKey(KeyCode.W)) 
+
+            if (isDashing)
             {
-                transform.position += Vector3.up * 5 * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.S)) 
-            {
-                transform.position += -Vector3.up * 5 * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.position += -Vector3.right * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.position += Vector3.right * speed * Time.deltaTime;
+                return;
             }
 
+            horizontal = Input.GetAxisRaw("Horizontal");
+
+            
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+            
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+            {
+                Debug.Log("Jumping");
+                rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            }
+            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+            {
+                StartCoroutine(Dash());
+            }
+
+            flip();
+    
+   
+}
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+       
+    }
+
+    private void flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
--- Assets/Scripts/PhotonScripts/Gameplaay/PlayerSpawn.cs
+++ Assets/Scripts/PhotonScripts/Gameplaay/PlayerSpawn.cs
@@ -6,9 +6,18 @@ using Photon.Pun;
 public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Vector2[] spawnPoints;
 
+    private static int currentSpawnIndex = 0;
     private void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(Random.Range(-6, 6), Random.Range(-6, 6)), Quaternion.identity);
+        // Get the local player actor number
+        int playerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1; // ActorNumber starts at 1, so subtract 1 for zero-based index
+
+        // Use the player index to get the corresponding spawn point
+        Vector2 spawnPosition = spawnPoints[playerIndex % spawnPoints.Length];
+
+        // Instantiate the player at the chosen spawn position
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
     }
 }
